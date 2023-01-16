40a914fee92452b297c499a08221e0d94bcee904
Artemis-the-gr8 <artemis.the.gr8@gmail.com>
2023-01-16 12:11:20 +0100

    Added test output to see a list of statistics/entities onServerStarted

--- src/main/java/io/github/artemis_the_gr8/playerstats/PlayerStats.java
+++ src/main/java/io/github/artemis_the_gr8/playerstats/PlayerStats.java
@@ -1,14 +1,19 @@
 package io.github.artemis_the_gr8.playerstats;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.stats.Stats;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -30,6 +35,14 @@ public class PlayerStats {
     private void setup(final FMLCommonSetupEvent event) {
         // Some preinit code
         LOGGER.info("Setting up PlayerStats" );
+    }
+
+    @SubscribeEvent
+    public void onServerStarted(ServerStartedEvent event) {
+//        AtomicInteger i = new AtomicInteger();
+//        Stats.CUSTOM.iterator().forEachRemaining(resourceLocationStat -> LOGGER.info(i.getAndIncrement() + ". " + resourceLocationStat.getName()));
+//        ForgeRegistries.ENTITIES.getEntries().forEach(resourceKeyEntityTypeEntry ->
+//                LOGGER.info(i.getAndIncrement() + ". " + resourceKeyEntityTypeEntry.getValue().toString()));
     }
 
     @Mod.EventBusSubscriber(modid = PlayerStats.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
