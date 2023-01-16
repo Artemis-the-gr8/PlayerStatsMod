542c2f00630c5c515a0a8b3b90a0aba8f58702b9
Artemis-the-gr8 <artemis.the.gr8@gmail.com>
2023-01-16 12:06:53 +0100

    Got the death and mine_block (dirt) stat to output value in chat on /playerstats command

--- src/main/java/io/github/artemis_the_gr8/playerstats/TestCommand.java
+++ src/main/java/io/github/artemis_the_gr8/playerstats/TestCommand.java
@@ -7,7 +7,12 @@ import net.minecraft.Util;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.registries.ForgeRegistries;
+
 
 public class TestCommand {
 
@@ -17,7 +22,15 @@ public class TestCommand {
 
     private static int execute(CommandContext<CommandSourceStack> command) {
         if(command.getSource().getEntity() instanceof Player player) {
-            player.sendMessage(new TextComponent("hello world"), Util.NIL_UUID);
+            if (player instanceof ServerPlayer serverPlayer) {
+                int deaths = serverPlayer.getStats().getValue(Stats.CUSTOM, Stats.DEATHS);
+                int mined_block = serverPlayer.getStats().getValue(Stats.BLOCK_MINED, ForgeRegistries.BLOCKS.getValue(Blocks.DIRT.getRegistryName()));
+
+                TextComponent msg = new TextComponent("You died " + deaths + " times");
+                TextComponent msg2 = new TextComponent("You mined " + mined_block + " dirt");
+                player.sendMessage(msg, Util.NIL_UUID);
+                player.sendMessage(msg2, Util.NIL_UUID);
+            }
         }
         return Command.SINGLE_SUCCESS;
     }
